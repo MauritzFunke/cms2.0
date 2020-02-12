51d8674b6226999c4df1e27bcbd1fb85302e2de1
BumseBine <mauritz.funke@gmail.com>
2020-02-12 10:24:58 -0600

    Added a scheduler manager

--- admin/app.ts
+++ admin/app.ts
@@ -3,6 +3,7 @@ import * as ejs from 'ejs';
 import * as session from 'express-session';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
+import * as partials from 'express-partials';
 import debug from './classes/debug';
 import Repo from './models/Repo';
 
@@ -15,26 +16,29 @@ const PORT = process.env.PORT || config.PORT;
 
 const app = express();
 
-mongoose.connect('mongodb://' + keys.mongo.user + ':'+keys.mongo.pwd +'@localhost:27017/website', (err) => {
+mongoose.connect('mongodb://' + keys.mongo.user + ':'+keys.mongo.pwd +'@localhost:27017/website', { useNewUrlParser: true, useUnifiedTopology: true } ,(err) => {
     if (err) throw err;
     debug.log('Connected to MongoDB');
-})
+});
 
 app.set('view engine', 'ejs');
+app.use(partials());
 app.use(session({
     secret: 'kittensareverymuchfancy',
     resave: false,
     saveUninitialized: false,
     cookie: { secure: true }
-}))
+}));
 app.use(bodyParser.json());
 app.use('/static/', express.static('./static/'));
 
 
 app.get('/', (req, res) => {
-    
+    res.render('sites/index');
 });
-
+app.get('/tasks', (req, res) => {
+    res.render('sites/tasks');
+})
 
 
 
---
+++ admin/classes/Server.ts
@@ -0,0 +1,15 @@
+const config = require('../config/vars');
+
+export default class Server {
+    private _id: String;
+    private IP: String;
+    private type: String;
+    private status: String;
+    private onlineTime: Number;
+    private offlineTime: Number;
+
+
+    constructor() {
+        
+    }
+};
